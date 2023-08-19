47a1187c77303bd629c1031f33822f75a4e2814d
frenemations <frenemations@gmail.com>
2023-08-19 16:57:45 +0200

    Initial commit for file sharing

---
+++ Progress/TestData.js
@@ -0,0 +1,202 @@
+data = [
+  {
+    "Sample": "01KM01B2",
+    "ID": "AAA",
+    "DrySampleWeightFull": 28.4,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM01B1",
+    "ID": "AAB",
+    "DrySampleWeightFull": 7.55,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM01S",
+    "ID": "AAC",
+    "DrySampleWeightFull": 12.85,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM01H",
+    "ID": "AAD",
+    "DrySampleWeightFull": 8.55,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "",
+    "Extracted": "",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM02B2",
+    "ID": "AAE",
+    "DrySampleWeightFull": 41.5,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM02B1",
+    "ID": "AAF",
+    "DrySampleWeightFull": 24.2,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM02S",
+    "ID": "AAG",
+    "DrySampleWeightFull": 35.05,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM02H",
+    "ID": "AAH",
+    "DrySampleWeightFull": 12.15,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM03B2",
+    "ID": "AAI",
+    "DrySampleWeightFull": "",
+    "DryExtractPowderWeight": "",
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM03B1",
+    "ID": "AAJ",
+    "DrySampleWeightFull": "",
+    "DryExtractPowderWeight": "",
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM03S",
+    "ID": "AAK",
+    "DrySampleWeightFull": "",
+    "DryExtractPowderWeight": "",
+    "FreezeDried": "",
+    "Powdered": "Y",
+    "Extracted": "",
+    "Lost": "Y"
+  },
+  {
+    "Sample": "01KM03H",
+    "ID": "AAL",
+    "DrySampleWeightFull": "",
+    "DryExtractPowderWeight": "",
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM04B2",
+    "ID": "AAM",
+    "DrySampleWeightFull": 13.35,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM04B1",
+    "ID": "AAN",
+    "DrySampleWeightFull": 19.45,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "",
+    "Powdered": "",
+    "Extracted": "",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM04S",
+    "ID": "AAO",
+    "DrySampleWeightFull": 31,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM04H",
+    "ID": "AAP",
+    "DrySampleWeightFull": 10.85,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM05B2",
+    "ID": "AAQ",
+    "DrySampleWeightFull": 37.5,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "",
+    "Extracted": "",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM05B1",
+    "ID": "AAR",
+    "DrySampleWeightFull": 8.8,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM05S",
+    "ID": "AAS",
+    "DrySampleWeightFull": 8.05,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  },
+  {
+    "Sample": "01KM05H",
+    "ID": "AAT",
+    "DrySampleWeightFull": 8.15,
+    "DryExtractPowderWeight": 1,
+    "FreezeDried": "Y",
+    "Powdered": "Y",
+    "Extracted": "Y",
+    "Lost": "N"
+  }
+]
